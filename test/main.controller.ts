9a9eee41598ada12706c3bc9d8214144ffb7f94d
祥和服务器 <guhuan4@outlook.com>
2023-05-07 22:19:50 +0800

    feat: update test prepare next day to test async

--- test/main.controller.ts
+++ test/main.controller.ts
@@ -3,10 +3,10 @@ import { GetMapping, RestController } from "../src/main";
 
 // 这里导出一个类 用controller装饰起来
 @RestController()
-export class Test {
+export class AppController {
   // 这里用get装饰起来
   @GetMapping()
-  getHello() {
+  public async getHello() {
     // 返回一个JSON对象
     return {
       statusCode: 200,
