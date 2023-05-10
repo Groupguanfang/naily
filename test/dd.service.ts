774ab185e03a0b6496132eea34d3c7b1d9a46a2f
Zero的Mac <guhuan4@icloud.com>
2023-05-10 16:10:53 +0800

    feat: delete test file

--- test/dd.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from "../src/main";
-import { CService } from "./cc.service";
-
-@Injectable
-export class DService {
-  public getText() {
-    return "访问到了DService";
-  }
-}
--- test/main.service.ts
+++ test/main.service.ts
@@ -1,13 +1,11 @@
 import { Injectable } from "../src/main";
-import { AService } from "./aa.service";
 
 // 使用Injectable标记这是一个可以被注入的类
 // 说人话就两个字：服务
 @Injectable
 export class AppService {
-  constructor(private readonly aService: AService) {}
   // 随便弄个方法
   getData() {
-    return this.aService.getText();
+    return "hello naily!";
   }
 }
